135c87f45464668c66339ac752d1ec5d0f9f755b
mrSemyon <syoma4k@gmail.com>
2022-12-29 19:29:04 +0500

    Made layout of the main page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,7 +3,3 @@
 @section('content')
 <h1>Hello! It's about page</h1>
 @endsection
-
-@section('sidebar')
-<h2>It's sidebar.</h2>
-@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,73 @@
 @extends('layout')
 
 @section('content')
-<h1>Hello! It's main page</h1>
+<div class="container">
+    <h1 align="center">My gallery</h1>
+    <div class="row">
+        <div class="col-md-3 px-4 py-2">
+            <img src="/laravel.jpg" class="img-thumbnail" alt="Laravel">
+            <div class="row justify-content-center">
+                <button type="button" class="m-2 btn btn-info">Info</button>
+                <button type="button" class="m-2 btn btn-warning">Edit</button>
+                <button type="button" class="m-2 btn btn-danger">Delete</button>
+            </div>
+        </div>
+        <div class="col-md-3 px-4 py-2">
+            <img src="/laravel.jpg" class="img-thumbnail" alt="Laravel">
+            <div class="row justify-content-center">
+                <button type="button" class="m-2 btn btn-info">Info</button>
+                <button type="button" class="m-2 btn btn-warning">Edit</button>
+                <button type="button" class="m-2 btn btn-danger">Delete</button>
+            </div>
+        </div>
+        <div class="col-md-3 px-4 py-2">
+            <img src="/laravel.jpg" class="img-thumbnail" alt="Laravel">
+            <div class="row justify-content-center">
+                <button type="button" class="m-2 btn btn-info">Info</button>
+                <button type="button" class="m-2 btn btn-warning">Edit</button>
+                <button type="button" class="m-2 btn btn-danger">Delete</button>
+            </div>
+        </div>
+        <div class="col-md-3 px-4 py-2">
+            <img src="/laravel.jpg" class="img-thumbnail" alt="Laravel">
+            <div class="row justify-content-center">
+                <button type="button" class="m-2 btn btn-info">Info</button>
+                <button type="button" class="m-2 btn btn-warning">Edit</button>
+                <button type="button" class="m-2 btn btn-danger">Delete</button>
+            </div>
+        </div>
+        <div class="col-md-3 px-4 py-2">
+            <img src="/laravel.jpg" class="img-thumbnail" alt="Laravel">
+            <div class="row justify-content-center">
+                <button type="button" class="m-2 btn btn-info">Info</button>
+                <button type="button" class="m-2 btn btn-warning">Edit</button>
+                <button type="button" class="m-2 btn btn-danger">Delete</button>
+            </div>
+        </div>
+        <div class="col-md-3 px-4 py-2">
+            <img src="/laravel.jpg" class="img-thumbnail" alt="Laravel">
+            <div class="row justify-content-center">
+                <button type="button" class="m-2 btn btn-info">Info</button>
+                <button type="button" class="m-2 btn btn-warning">Edit</button>
+                <button type="button" class="m-2 btn btn-danger">Delete</button>
+            </div>
+        </div>
+        <div class="col-md-3 px-4 py-2 ">
+            <img src="/laravel.jpg" class="img-thumbnail" alt="Laravel">
+            <div class="row justify-content-center">
+                <button type="button" class="m-2 btn btn-info">Info</button>
+                <button type="button" class="m-2 btn btn-warning">Edit</button>
+                <button type="button" class="m-2 btn btn-danger">Delete</button>
+            </div>
+        </div>
+        <div class="col-md-3 px-4 py-2">
+            <img src="/laravel.jpg" class="img-thumbnail" alt="Laravel">
+            <div class="row justify-content-center">
+                <button type="button" class="m-2 btn btn-info">Info</button>
+                <button type="button" class="m-2 btn btn-warning">Edit</button>
+                <button type="button" class="m-2 btn btn-danger">Delete</button>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
